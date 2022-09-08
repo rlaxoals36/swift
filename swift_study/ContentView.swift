4128de3d39aad853b2a61b3aca5b1dd5d42593a1
rlaxoals36 <gimtaemin36@gmail.com>
2022-09-08 13:35:25 +0900

    test

---
+++ swift_study/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swift_study
+//
+//  Created by Taemin Kim on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        // ddd
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_study/swift_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_studyApp.swift
+//  swift_study
+//
+//  Created by Taemin Kim on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct swift_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
